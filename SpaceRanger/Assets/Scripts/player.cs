69aea2dba115f7545b6afb5b45d50c803af28438
MKDev121 <meatk121@gmail.com>
2025-03-07 19:26:38 +0530

    Update

--- SpaceRanger/Assets/Scripts/player.cs
+++ SpaceRanger/Assets/Scripts/player.cs
@@ -13,7 +13,7 @@ public class player : MonoBehaviour
     public float bg_speed;
     Camera cam;
     public GameObject background;
-    public List<GameObject> bgs;
+
     List<bool> truth_values;
     float timer;
     int score;
@@ -29,13 +29,9 @@ public class player : MonoBehaviour
     void Start()
     {
         cam=GameObject.Find("Main Camera").GetComponent<Camera>();
-        bgs.Add(GameObject.Find("space1"));
         gameOverScreen=GameObject.Find("Canvas").transform.GetChild(2).gameObject;
         if(HighScoreText!=null)
-         HighScoreText.text=PlayerPrefs.GetInt("HighScore",0).ToString();
-        
-        
-
+            HighScoreText.text=PlayerPrefs.GetInt("HighScore",0).ToString();
     }
 
     // Update is called once per frame
@@ -43,18 +39,8 @@ public class player : MonoBehaviour
     {
 
         if(!gameover&&gameStart){
-            
-            
-            
-            float horizontal=Input.GetAxis("Horizontal");
-            float vertical=Input.GetAxis("Vertical");
-            
-            Vector3 movement=new Vector3(horizontal,vertical,0)*speed*Time.deltaTime;
-            transform.position+=movement;  
-            transform.position=Clamping(transform.position);
-
+            movement();
             shoot();
-
             if(timer>=0f)
                 timer-=Time.deltaTime;
             else{
@@ -66,26 +52,40 @@ public class player : MonoBehaviour
 
         }
         score_txt.text=score.ToString();
-            if(Input.GetKeyDown("space")){
-                GameObject pause_menu=GameObject.Find("Canvas").transform.GetChild(1).gameObject;
-                if(!gameover&&gameStart){
-                    if(!paused){
-                        pause_menu.SetActive(true);
 
-                        paused=true;
-                    }
-                    else{
-                        pause_menu.SetActive(false);
-                        paused=false;
-                    }
-                }
-            }
         if(gameover){
             gameObject.GetComponent<SpriteRenderer>().enabled=false;     
             gameOverScreen.SetActive(true);
             if(score>PlayerPrefs.GetInt("HighScore")){
                 PlayerPrefs.SetInt("HighScore",score);
                 PlayerPrefs.Save();
+            }
+        }
+
+    }
+    Vector3 Clamping(Vector3 position){
+        return new Vector3(Mathf.Clamp(position.x,-8,8),Mathf.Clamp(position.y,-4.2f,4.2f),0);
+    }
+    void movement(){
+        float horizontal=Input.GetAxis("Horizontal");
+        float vertical=Input.GetAxis("Vertical");
+        Vector3 movement=new Vector3(horizontal,vertical,0)*speed*Time.deltaTime;
+        transform.position+=movement;  
+        transform.position=Clamping(transform.position);
+    }
+    
+    void pausing(){
+        if(Input.GetKeyDown("space")){
+            GameObject pause_menu=GameObject.Find("Canvas").transform.GetChild(1).gameObject;
+            if(!gameover&&gameStart){
+                if(!paused){
+                    pause_menu.SetActive(true);
+                    paused=true;
+                }
+                else{
+                    pause_menu.SetActive(false);
+                    paused=false;
+                }
             }
         }
         if(paused){
@@ -95,9 +95,7 @@ public class player : MonoBehaviour
         else
             Time.timeScale=1f;  
     }
-    Vector3 Clamping(Vector3 position){
-        return new Vector3(Mathf.Clamp(position.x,-8,8),Mathf.Clamp(position.y,-4.2f,4.2f),0);
-    }
+
     void shoot(){
         if(Input.GetMouseButtonDown(0)){
             Debug.Log("Shoot");
